7cedd77eb3404b4adceede8f9b76bca80eb71e67
Sriramprasath31 <sriramprasath31@gmail.com>
2026-08-30 15:16:19 +0530

    Added Carry Look-Ahead Adder

---
+++ day_03/04_carry_lookahead_adder/carry_look_tb.v
@@ -0,0 +1,27 @@
+module tb;
+
+    reg [3:0] A, B;
+    reg Cin;
+    wire [3:0] Sum;
+    wire Cout;
+
+    carry_lookahead_adder uut (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
+
+    initial begin
+        $monitor("A=%b B=%b Cin=%b | Sum=%b Cout=%b",
+                  A, B, Cin, Sum, Cout);
+
+        A = 4'b0000; B = 4'b0000; Cin = 0;
+        #10 A = 4'b0101; B = 4'b0011; Cin = 0;
+        #10 A = 4'b1111; B = 4'b0001; Cin = 0;
+        #10 A = 4'b1010; B = 4'b0101; Cin = 1;
+        #10 $finish;
+    end
+
+endmodule
